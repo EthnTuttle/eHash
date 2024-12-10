8b177aadc10bd8448ce05cb5fa084d4b33907c56
Gary Krause <gary.krause@mara.com>
2024-12-09 19:24:47 -0500

    maybe imple Sv2DataType

--- src/data_types/blinded_message_share.rs
+++ src/data_types/blinded_message_share.rs
@@ -20,8 +20,6 @@ use core::convert::TryInto;
 #[already_sized]
 #[repr(C)]
 pub struct BlindedMessageShare<'decoder> {
-    #[cfg_attr(feature = "with_serde", serde(borrow))]
-    pub extranonce: B032<'decoder>,
     pub job_id: u64,
     pub nonce: u32,
     pub ntime: u32,
@@ -31,10 +29,12 @@ pub struct BlindedMessageShare<'decoder> {
     pub keyset_id: u64,
     pub parity_bit: bool,
     pub blinded_secret: PubKey<'decoder>,
+    #[cfg_attr(feature = "with_serde", serde(borrow))]
+    pub extranonce: B032<'decoder>,
 }
 
-impl<'a> Share<'a> {
-    pub fn seq_into_encodable_field(v: Seq064K<'a, Share<'a>>) -> EncodableField<'a> {
+impl<'a> BlindedMessageShare<'a> {
+    pub fn seq_into_encodable_field(v: Seq064K<'a, BlindedMessageShare<'a>>) -> EncodableField<'a> {
         let inner = v.into_inner();
         let inner_len = inner.len() as u16;
         let mut as_encodable: Vec<EncodableField> = Vec::with_capacity((inner_len + 2) as usize);
@@ -51,36 +51,41 @@ impl<'a> Share<'a> {
     }
 }
 
-impl<'d> GetMarker for Share<'d> {
+impl<'d> GetMarker for BlindedMessageShare<'d> {
     fn get_marker() -> FieldMarker {
         let markers = vec![
+            u64::get_marker(),
             u32::get_marker(),
             u32::get_marker(),
             u32::get_marker(),
+            u64::get_marker(),
+            u64::get_marker(),
+            bool::get_marker(),
+            PubKey::get_marker(),
             B032::get_marker(),
-            u64::get_marker(),
-            u64::get_marker(),
-            u32::get_marker(),
-            B064K::get_marker(),
         ];
         FieldMarker::Struct(markers)
     }
 }
 
-impl<'d> GetSize for Share<'d> {
+impl<'d> GetSize for BlindedMessageShare<'d> {
     fn get_size(&self) -> usize {
-        self.nonce.get_size()
-            + self.ntime.get_size()
-            + self.version.get_size()
-            + self.extranonce.get_size()
+        self.extranonce.get_size()
             + self.job_id.get_size()
+            + self.nonce.get_size()
+            + self.merkle_path.get_size()
+            + self.ntime.get_size()
             + self.reference_job_id.get_size()
             + self.share_index.get_size()
-            + self.merkle_path.get_size()
+            + self.version.get_size()
+            + self.amount.get_size()
+            + self.keyset_id.get_size()
+            + self.parity_bit.get_size()
+            + self.blinded_secret.get_size()
     }
 }
 
-impl<'d> binary_sv2::SizeHint for Share<'d> {
+impl<'d> binary_sv2::SizeHint for BlindedMessageShare<'d> {
     // This is not needed
     fn size_hint(_: &[u8], _: usize) -> Result<usize, binary_sv2::Error> {
         todo!()
@@ -90,33 +95,37 @@ impl<'d> binary_sv2::SizeHint for Share<'d> {
     }
 }
 
-impl<'d> TryInto<binary_sv2::decodable::FieldMarker> for Share<'d> {
+// TODO: test this
+impl<'d> TryInto<binary_sv2::decodable::FieldMarker> for BlindedMessageShare<'d> {
     type Error = ();
     fn try_into(self) -> Result<binary_sv2::decodable::FieldMarker, Self::Error> {
-        Ok(Share::get_marker())
+        Ok(BlindedMessageShare::get_marker())
     }
 }
-impl<'d> Sv2DataType<'d> for Share<'d> {
+
+impl<'d> Sv2DataType<'d> for BlindedMessageShare<'d> {
     fn from_bytes_unchecked(data: &'d mut [u8]) -> Self {
-        let nonce = u32::from_bytes_unchecked(&mut data[0..4]);
-        let ntime = u32::from_bytes_unchecked(&mut data[4..8]);
-        let version = u32::from_bytes_unchecked(&mut data[8..12]);
-        let extranonce = B032::from_bytes_unchecked(&mut data[12..]);
+        let job_id = u64::from_bytes_unchecked(&mut data[0..8]);
+        let nonce = u32::from_bytes_unchecked(&mut data[8..12]);
+        let ntime = u32::from_bytes_unchecked(&mut data[12..16]);
+        let version = u32::from_bytes_unchecked(&mut data[16..20]);
+        let amount = u64::from_bytes_unchecked(&mut data[20..28]);
+        let keyset_id = u64::from_bytes_unchecked(&mut data[28..36]);
+        let parity_bit = bool::from_bytes_unchecked(&mut data[36..37]);
+        let blinded_secret = PubKey
+            ::from_bytes_unchecked(&mut data[37..69]);
+        let extranonce = B032::from_bytes_unchecked(&mut data[69..]);
         let extranonce = extranonce.into_static();
-        let index = extranonce.len() + 12;
-        let job_id = u64::from_bytes_unchecked(&mut data[index..index + 8]);
-        let reference_job_id = u64::from_bytes_unchecked(&mut data[index + 8..index + 16]);
-        let share_index = u32::from_bytes_unchecked(&mut data[index + 16..]);
-        let merkle_path = B064K::from_bytes_unchecked(&mut data[index + 16..]);
         Self {
+            job_id,
             nonce,
             ntime,
             version,
+            amount,
+            keyset_id,
+            parity_bit,
+            blinded_secret,
             extranonce,
-            job_id,
-            reference_job_id,
-            share_index,
-            merkle_path,
         }
     }
 
@@ -130,16 +139,14 @@ impl<'d> Sv2DataType<'d> for Share<'d> {
 
     fn to_slice_unchecked(&'d self, dst: &mut [u8]) {
         debug_assert!(dst.len() == 60);
-        self.nonce.to_slice_unchecked(&mut dst[0..4]);
-        self.ntime.to_slice_unchecked(&mut dst[4..12]);
-        self.version.to_slice_unchecked(&mut dst[8..12]);
-        self.extranonce.to_slice_unchecked(&mut dst[12..]);
-        let index = 12 + self.extranonce.get_size();
-        self.job_id.to_slice_unchecked(&mut dst[index..index + 8]);
-        self.reference_job_id
-            .to_slice_unchecked(&mut dst[index + 8..index + 16]);
-        self.share_index
-            .to_slice_unchecked(&mut dst[index + 16..index + 20]);
-        self.merkle_path.to_slice_unchecked(&mut dst[index + 20..]);
+        self.job_id.to_slice_unchecked(&mut dst[0..8]);
+        self.nonce.to_slice_unchecked(&mut dst[8..12]);
+        self.ntime.to_slice_unchecked(&mut dst[12..16]);
+        self.version.to_slice_unchecked(&mut dst[16..20]);
+        self.amount.to_slice_unchecked(&mut dst[20..28]);
+        self.keyset_id.to_slice_unchecked(&mut dst[28..36]);
+        self.parity_bit.to_slice_unchecked(&mut dst[36..37]);
+        self.blinded_secret.to_slice_unchecked(&mut dst[37..69]);
+        self.extranonce.to_slice_unchecked(&mut dst[69..]);
     }
 }
